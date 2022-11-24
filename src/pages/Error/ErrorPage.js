6528c6b94b7663a88f6c73bfc54bc8d00245246d
islammd39e <islammdrahat308@gmail.comm>
2022-11-24 13:31:06 +0600

    upload 404 page

---
+++ src/pages/Error/ErrorPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ErrorPage = () => {
+  return (
+    <div className="text-center my-10">
+      <h2 className="font-semibold text-4xl">Page Not Found</h2>
+      <h2 className="font-bold text-7xl text-red-500">404</h2>
+    </div>
+  );
+};
+
+export default ErrorPage;
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main/Main";
+import ErrorPage from "../pages/Error/ErrorPage";
 import Home from "../pages/Home/Home/Home";
 
 export const router = createBrowserRouter([
@@ -10,6 +11,10 @@ export const router = createBrowserRouter([
             {
                 path:"/",
                 element:<Home></Home>
+            },
+            {
+                path:"*",
+                element:<ErrorPage></ErrorPage>
             }
         ]
     }
