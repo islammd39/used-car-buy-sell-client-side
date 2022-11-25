a537ef5360b7750e8d61ff90ec8f159cef1ce310
islammd39e <islammdrahat308@gmail.comm>
2022-11-25 17:36:51 +0600

    readme file edite

--- src/pages/Home/Product/CategoriesSection.js
+++ src/pages/Home/Product/CategoriesSection.js
@@ -5,7 +5,7 @@ const CategoriesSection = ({ pCategory }) => {
     const {name, id} = pCategory;
   // console.log(pCategory);
   return (
-    <div>
+    <div className="mx-5">
       <div className="card w-96 bg-base-100 shadow-xl">
         <div className="card-body">
           <p><Link to={`pCategory/${id}`}>{name}</Link></p>
